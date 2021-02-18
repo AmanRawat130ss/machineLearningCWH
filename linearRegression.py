e9576f6e18db1382f312fa5f1d46319eadeafcdb
AmanRawat130ss <amnrawat130ss@gmail.com>
2021-02-18 22:52:46 +0530

    new add

--- linearRegression.py
+++ linearRegression.py
@@ -11,8 +11,6 @@ diabetes = datasets.load_diabetes()
 # diabetes_X = diabetes.data[:, np.newaxis, 2]
 diabetes_X = diabetes.data
 
-
-print(diabetes_X)
 diabetes_X_train = diabetes_X[:-30]
 diabetes_X_test = diabetes_X[-30:]
 
